94fecf8784580dfb576b7de6df5e54ec5c440641
G. SURYA TEJA <69040008+gsuryateja2000@users.noreply.github.com>
2023-06-12 13:07:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
 const express = require('express');
 const app = express();
-const { MongoClient } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
-// Set up middleware for parsing JSON in request bodies
-app.use(express.json());
+const uri = 'mongodb+srv://suryagunji24:Surya2000@cluster0.uma9ypy.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Define your API endpoint for receiving form data
-app.post('/api/messages', async (req, res) => {
-  const name = req.body.name;
-  const message = req.body.message;
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-  try {
-    // Connect to MongoDB Atlas
-    const client = new MongoClient('mongodb+srv://suryagunji24:Surya2000@cluster0.uma9ypy.mongodb.net/?retryWrites=true&w=majority');
-    await client.connect();
+app.post('/submit', async (req, res) => {
+const { name, message } = req.body;
 
-    // Insert the data into MongoDB Atlas
-    const collection = client.db('chatgpt').collection('notes');
-    await collection.insertOne({ name, message });
+try {
+await client.connect();
+const db = client.db('details');
+const collection = db.collection('notes');
 
-    // Close the MongoDB connection
-    await client.close();
+await collection.insertOne({ name, message });
 
-    // Respond with a success message
-    res.status(200).json({ message: 'Data stored successfully' });
-  } catch (error) {
-    // Handle errors and respond with an error message
-    console.error(error);
-    res.status(500).json({ message: 'Error storing data' });
-  }
+console.log('Data inserted into MongoDB');
+res.send('Data submitted successfully');
+} catch (error) {
+console.error('Failed to insert data into MongoDB', error);
+res.status(500).send('An error occurred');
+}
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log(`Server listening on port ${PORT}`);
 });
+
+
+
+
+
+// mongodb+srv://suryagunji24:Surya2000@cluster0.uma9ypy.mongodb.net/dbcnc
